6372c111044d784adf0df3d3b458575a3b737d72
luciano <luciano.bj23@hotmail.com>
2023-08-26 16:04:29 +0100

    include PID on interface

--- uav_land/scripts/parameters_node.py
+++ uav_land/scripts/parameters_node.py
@@ -23,36 +23,40 @@ class ControllerGUI:
         
         pd_radio = Radiobutton(self.root, text="PD", variable=self.controller_mode, value="PD")
         pd_radio.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-        cascade_radio = Radiobutton(self.root, text="Cascade", variable=self.controller_mode, value="Cascade")
-        cascade_radio.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-        parallel_radio = Radiobutton(self.root, text="Parallel", variable=self.controller_mode, value="Parallel")
-        parallel_radio.grid(row=11, column=0, padx=10, pady=5, sticky="w")
+        pid_radio = Radiobutton(self.root, text="PID", variable=self.controller_mode, value="PID")
+        pid_radio.grid(row=0, column=10, padx=10, pady=5, sticky="w")
 
-        self.create_gain_entries("PD", ["P", "D"], 2)
-        self.create_gain_entries("Cascade", ["pdP", "pdD", "piP", "piI"], 6)
-        self.create_gain_entries("Parallel", ["pdP", "pdD", "piP", "piI"], 12)
+        cascade_radio = Radiobutton(self.root, text="Cascade", variable=self.controller_mode, value="Cascade")
+        cascade_radio.grid(row=10, column=0, padx=10, pady=5, sticky="w")
+        parallel_radio = Radiobutton(self.root, text="Parallel", variable=self.controller_mode, value="Parallel")
+        parallel_radio.grid(row=10, column=10, padx=10, pady=5, sticky="w")
+        
+        self.create_gain_entries("PD", ["P", "D"], 2, 0)
+        self.create_gain_entries("PID", ["P", "I", "D"], 2, 11)
+        self.create_gain_entries("Cascade", ["pdP", "pdD", "piP", "piI"], 10, 0)
+        self.create_gain_entries("Parallel", ["pdP", "pdD", "piP", "piI"], 10, 11)
 
         self.submit_button = Button(self.root, text="Aplicar", command=self.apply_gains)
-        self.submit_button.grid(row=17, column=0, columnspan=15, padx=10, pady=10)
+        self.submit_button.grid(row=18, column=0, columnspan=15, padx=10, pady=10)
 
         self.save_button = Button(self.root, text="save", command=self.save_gains)
-        self.save_button.grid(row=17, column=2, columnspan=15, padx=10, pady=10)
+        self.save_button.grid(row=18, column=2, columnspan=15, padx=10, pady=10)
 
-    def create_gain_entries(self, controller_type, gains_Text, row_start):
+    def create_gain_entries(self, controller_type, gains_Text, row_start, column_start):
         dimensions = ["x", "y", "z", "yaw"]
 
         for i, gain in enumerate(gains_Text):
             label = Label(self.root, text=gain)
-            label.grid(row=row_start + i + 1, column=0, sticky="e")
+            label.grid(row=row_start + i + 1, column=column_start, sticky="e")
 
         for j, dimension in enumerate(dimensions):
             label_dimension = Label(self.root, text=dimension)
-            label_dimension.grid(row=row_start, column=j + 1)
+            label_dimension.grid(row=row_start, column= column_start + j + 1)
 
             for i, gain in enumerate(gains_Text):
                 entry = Entry(self.root, width=5)
                 entry.insert(0, "0.0")
-                entry.grid(row=row_start + i + 1, column=j + 1, padx=5, pady=5)
+                entry.grid(row=row_start + i + 1, column= column_start + j + 1, padx=5, pady=5)
                 self.entries[f"{controller_type}_{gain}_{dimension}"] = entry
 
     def save_gains(self):
@@ -90,6 +94,19 @@ class ControllerGUI:
         self.gains.pd_ctrl.z.d_gain   = float(self.get_entry("PD", "D", "z"  ).get())
         self.gains.pd_ctrl.yaw.p_gain = float(self.get_entry("PD", "P", "yaw").get())
         self.gains.pd_ctrl.yaw.d_gain = float(self.get_entry("PD", "D", "yaw").get())
+
+        self.gains.pid_ctrl.x.p_gain   = float(self.get_entry("PID", "P", "x"  ).get())
+        self.gains.pid_ctrl.x.i_gain   = float(self.get_entry("PID", "I", "x"  ).get())
+        self.gains.pid_ctrl.x.d_gain   = float(self.get_entry("PID", "D", "x"  ).get())
+        self.gains.pid_ctrl.y.p_gain   = float(self.get_entry("PID", "P", "y"  ).get())
+        self.gains.pid_ctrl.y.i_gain   = float(self.get_entry("PID", "I", "y"  ).get())
+        self.gains.pid_ctrl.y.d_gain   = float(self.get_entry("PID", "D", "y"  ).get())
+        self.gains.pid_ctrl.z.p_gain   = float(self.get_entry("PID", "P", "z"  ).get())
+        self.gains.pid_ctrl.z.i_gain   = float(self.get_entry("PID", "I", "z"  ).get())
+        self.gains.pid_ctrl.z.d_gain   = float(self.get_entry("PID", "D", "z"  ).get())
+        self.gains.pid_ctrl.yaw.p_gain = float(self.get_entry("PID", "P", "yaw").get())
+        self.gains.pid_ctrl.yaw.i_gain = float(self.get_entry("PID", "I", "yaw").get())
+        self.gains.pid_ctrl.yaw.d_gain = float(self.get_entry("PID", "D", "yaw").get())
 
         self.gains.cascade_ctrl.x.pd_ctrl.p_gain = float(self.get_entry("Cascade", "pdP", "x").get())
         self.gains.cascade_ctrl.x.pd_ctrl.d_gain = float(self.get_entry("Cascade", "pdD", "x").get())
@@ -134,10 +151,12 @@ class ControllerGUI:
         self.gains.mode = 0
         if self.controller_mode.get() == "PD":
             self.gains.mode = 1
-        elif self.controller_mode.get() == "Cascade":
+        if self.controller_mode.get() == "PID":
             self.gains.mode = 2
-        elif self.controller_mode.get() == "Parallel":
+        elif self.controller_mode.get() == "Cascade":
             self.gains.mode = 3
+        elif self.controller_mode.get() == "Parallel":
+            self.gains.mode = 4
 
 
         self.par_pub.publish(self.gains)
