5a0037a55704387efccc47758e39449898eb4b09
luciano <luciano.bj23@hotmail.com>
2023-09-11 18:34:50 +0100

    Land_Controller::update_parameters

--- uav_land/include/uav_land/land_controller.cpp
+++ uav_land/include/uav_land/land_controller.cpp
@@ -21,6 +21,47 @@ void Land_Controller::print_parameters()
 
 void Land_Controller::update_parameters(uav_land::controllers_gain newParameters)
 {
+    pdController.update_x(newParameters.pd_ctrl.x.p_gain, newParameters.pd_ctrl.x.d_gain);
+    pdController.update_y(newParameters.pd_ctrl.y.p_gain, newParameters.pd_ctrl.y.d_gain);
+    pdController.update_z(newParameters.pd_ctrl.z.p_gain, newParameters.pd_ctrl.z.d_gain);
+    pdController.update_theta(newParameters.pd_ctrl.yaw.p_gain, newParameters.pd_ctrl.yaw.d_gain);
+
+    cascadeController.update_x(newParameters.cascade_ctrl.x.pd_ctrl.p_gain,
+                               newParameters.cascade_ctrl.x.pd_ctrl.d_gain,
+                               newParameters.cascade_ctrl.x.pi_ctrl.p_gain,
+                               newParameters.cascade_ctrl.x.pi_ctrl.i_gain);
+    cascadeController.update_y(newParameters.cascade_ctrl.y.pd_ctrl.p_gain,
+                               newParameters.cascade_ctrl.y.pd_ctrl.d_gain,
+                               newParameters.cascade_ctrl.y.pi_ctrl.p_gain,
+                               newParameters.cascade_ctrl.y.pi_ctrl.i_gain);
+    cascadeController.update_z(newParameters.cascade_ctrl.z.pd_ctrl.p_gain,
+                               newParameters.cascade_ctrl.z.pd_ctrl.d_gain,
+                               newParameters.cascade_ctrl.z.pi_ctrl.p_gain,
+                               newParameters.cascade_ctrl.z.pi_ctrl.i_gain);
+    cascadeController.update_theta(newParameters.cascade_ctrl.yaw.pd_ctrl.p_gain,
+                                   newParameters.cascade_ctrl.yaw.pd_ctrl.d_gain,
+                                   newParameters.cascade_ctrl.yaw.pi_ctrl.p_gain,
+                                   newParameters.cascade_ctrl.yaw.pi_ctrl.i_gain);
+
+    parallelController.update_x(newParameters.paralel_ctrl.x.pd_ctrl.p_gain,
+                                newParameters.paralel_ctrl.x.pd_ctrl.d_gain,
+                                newParameters.paralel_ctrl.x.pi_ctrl.p_gain,
+                                newParameters.paralel_ctrl.x.pi_ctrl.i_gain);
+    parallelController.update_y(newParameters.paralel_ctrl.y.pd_ctrl.p_gain,
+                                newParameters.paralel_ctrl.y.pd_ctrl.d_gain,
+                                newParameters.paralel_ctrl.y.pi_ctrl.p_gain,
+                                newParameters.paralel_ctrl.y.pi_ctrl.i_gain);
+    parallelController.update_z(newParameters.paralel_ctrl.z.pd_ctrl.p_gain,
+                                newParameters.paralel_ctrl.z.pd_ctrl.d_gain,
+                                newParameters.paralel_ctrl.z.pi_ctrl.p_gain,
+                                newParameters.paralel_ctrl.z.pi_ctrl.i_gain);
+    parallelController.update_theta(newParameters.paralel_ctrl.yaw.pd_ctrl.p_gain,
+                                    newParameters.paralel_ctrl.yaw.pd_ctrl.d_gain,
+                                    newParameters.paralel_ctrl.yaw.pi_ctrl.p_gain,
+                                    newParameters.paralel_ctrl.yaw.pi_ctrl.i_gain);
+
+    controller_mode = newParameters.mode;
+    setpoint.z = newParameters.altitude;
 }
 
 geometry_msgs::Twist Land_Controller::get_velocity(geometry_msgs::PoseStamped poseStamped, Speed drone_vel)
