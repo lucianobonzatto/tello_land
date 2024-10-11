0f7ba029ce84d3619bf5e6ab9fac3ae43c702698
luciano <luciano.bj23@hotmail.com>
2024-10-11 00:22:14 -0300

    LandpadFrameToCameraFrame

--- uav_land/scripts/read_guido.py
+++ uav_land/scripts/read_guido.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import cv2
-import rospy
 import rosbag
 import cv2.aruco as aruco
 import numpy as np
+import math
 
 from cv_bridge import CvBridge, CvBridgeError
 from scipy.spatial.transform import Rotation as R
+
 
 class BagReader:
     def __init__(self, bag_filename):
@@ -37,16 +38,13 @@ class BagReader:
         try:
             with rosbag.Bag(self.bag_filename, "r") as bag:
                 for topic, msg, t in bag.read_messages():
-                    print(t, end="\r")
+                    # print(t, end="\r")
                     self.topic_treatment(topic, msg, t)
-                    if rospy.is_shutdown():
-                        bag.close()
-                        return -1
 
                 bag.close()
 
         except rosbag.ROSBagException as e:
-            rospy.logerr("Erro ao reproduzir o arquivo de bag: %s", str(e))
+            print("Erro ao reproduzir o arquivo de bag: %s", str(e))
 
     def topic_treatment(self, topic, msg, t):
         if topic == "/camera/image_raw":
@@ -61,7 +59,7 @@ class BagReader:
                     (i, id[0]) for i, id in enumerate(ids) if id[0] in self.marker_sizes
                 ]
                 #############
-                frame = aruco.drawDetectedMarkers(frame, corners)
+                # frame = aruco.drawDetectedMarkers(frame, corners)
                 #############
 
                 for i, marker_id in ids_to_process:
@@ -73,18 +71,18 @@ class BagReader:
 
                     if rvecs is not None and tvecs is not None:
                         #############
-                        frame = cv2.drawFrameAxes(
-                            frame,self.camera_matrix,self.distortion_coeffs,
-                            rvecs[0],tvecs[0],marker_length,
-                        )
+                        # frame = cv2.drawFrameAxes(
+                        #     frame,self.camera_matrix,self.distortion_coeffs,
+                        #     rvecs[0],tvecs[0],marker_length,
+                        # )
 
-                        distance = np.linalg.norm(tvecs[0])
-                        text = f"ID {marker_id}: {distance:.2f} m"
-                        corner = tuple(corners[i][0][0].astype(int))
-                        cv2.putText(
-                            frame,text,corner,cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5,(255, 0, 0),2,
-                        )
+                        # distance = np.linalg.norm(tvecs[0])
+                        # text = f"ID {marker_id}: {distance:.2f} m"
+                        # corner = tuple(corners[i][0][0].astype(int))
+                        # cv2.putText(
+                        #     frame,text,corner,cv2.FONT_HERSHEY_SIMPLEX,
+                        #     0.5,(255, 0, 0),2,
+                        # )
                         #############
                         tvecs = np.squeeze(tvecs)
                         rvecs = np.squeeze(rvecs)
@@ -101,11 +99,74 @@ class BagReader:
                         }
                         self.data = pd.concat([self.data, pd.DataFrame([data_row])], ignore_index=True)
 
+                        result = self.LandpadFrameToCameraFrame(
+                            [data_row["Tx (m)"],   data_row["Ty (m)"],   data_row["Tz (m)"], 
+                             data_row["Rx (deg)"], data_row["Ry (deg)"], data_row["Rz (deg)"]],
+                            data_row["Marker ID"],
+                        )
+                        print(result[2], data_row["Tz (m)"])
+
             cv2.imshow("Detected ArUco markers", frame)
             cv2.waitKey(1)
 
+    def LandpadFrameToCameraFrame(self, P, id):
+
+        if id not in [272, 682, 0]:
+            return np.array([-999.0, -999.0, -999.0]), np.array([-999.0, -999.0, -999.0])
+
+        Tvec = np.array([P[0], P[1], P[2]])
+        Rvec = np.array([P[3], P[4], P[5]])
+        r = R.from_rotvec(Rvec)
+
+        TransformationMatrixCameraToAruco = np.eye(4)
+        TransformationMatrixCameraToAruco[:3, :3] = r.as_matrix()
+        TransformationMatrixCameraToAruco[:3, 3] = Tvec
+
+        PAr272 = np.array([0.320, 0.215, 0])
+        PAr682 = np.array([0.043, 0.038, 0])
+        PAr000 = np.array([-0.255, -0.160, 0])
+
+        TransformationMatrixArucoToLandpad = np.eye(4)
+        if id == 272:
+            TransformationMatrixArucoToLandpad[0, 0] = -1
+            TransformationMatrixArucoToLandpad[1, 1] = -1
+            TransformationMatrixArucoToLandpad[:3, 3] = PAr272
+        elif id == 682:
+            TransformationMatrixArucoToLandpad[:3, 3] = PAr682
+        elif id == 0:
+            TransformationMatrixArucoToLandpad[0, 0] = -1
+            TransformationMatrixArucoToLandpad[1, 1] = -1
+            TransformationMatrixArucoToLandpad[:3, 3] = PAr000
+
+        TransformationMatrixCameraToLandpad = np.matmul(
+            TransformationMatrixCameraToAruco,
+            np.linalg.inv(TransformationMatrixArucoToLandpad),
+        )
+
+        PCamera = np.array([P[0], P[1], P[2], 1])
+        PLandpadFrame = np.matmul(TransformationMatrixCameraToLandpad, PCamera)
+
+        Delta = np.array([-999.0, -999.0, -999.0, 1])
+        if id == 272:
+            Delta[0] = PLandpadFrame[0] - PAr272[0]
+            Delta[1] = PLandpadFrame[1] - PAr272[1]
+            Delta[2] = 0
+        elif id == 682:
+            Delta[0] = PLandpadFrame[0] - PAr682[0]
+            Delta[1] = PLandpadFrame[1] - PAr682[1]
+            Delta[2] = 0
+        elif id == 0:
+            Delta[0] = PLandpadFrame[0] - PAr000[0]
+            Delta[1] = PLandpadFrame[1] - PAr000[1]
+            Delta[2] = 0
+
+        PCenterCameraFrame = np.matmul(
+            np.linalg.inv(TransformationMatrixCameraToLandpad), np.array(Delta)
+        )
+
+        return PCenterCameraFrame[:3]
+
 def main():
-    rospy.init_node("read_bag_node", anonymous=True)
     app = BagReader("/home/lukn23/Desktop/rgb/teste.bag")
     app.show_bag()
     app.save_to_csv('dados_posicoes.csv')
